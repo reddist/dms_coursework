d6b89282fdd8587736f1f75775eced5d0016904e
Aleksey Vaskin <erddist@gmail.com>
2022-01-22 00:33:34 +0300

    Убрал пароли от БД.

--- server/db_model.js
+++ server/db_model.js
@@ -1,18 +1,20 @@
 const Pool = require('pg').Pool
-// let pool = new Pool({
-//     database: 'coursework',
-//     port: 5432,
-//     host: 'localhost'
-// });
+let pool = new Pool({
+    database: 'coursework',
+    port: 5432,
+    host: 'localhost',
+    user: '',
+    password: '',
+});
 
 
 //helios
-let pool = new Pool({
-    database: 'studs',
-    port: 5678,
-    host: 'localhost',
-    user: 's265058',
-    password: 'wnz862'
-});
+// let pool = new Pool({
+//     database: 'studs',
+//     port: 5678,
+//     host: 'localhost',
+//     user: '',
+//     password: ''
+// });
 
 module.exports = pool;
